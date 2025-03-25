1c12c7b23d09044648405bf2d84d4320887cb316
jorge-ysart <jorge.ysart@makingscience.com>
2025-03-25 16:28:04 +0100

    Mi mensaje de commit

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+// 👉 Equivalente a __dirname en ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
